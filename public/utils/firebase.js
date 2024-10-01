8afb86dd152f40de25d0e1b0df4a8ce5129900e0
savio-almeida <142703071+savio-almeida@users.noreply.github.com>
2024-09-30 21:29:50 -0300

    Update firebase.js

--- public/utils/firebase.js
+++ public/utils/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 import { getAnalytics, isSupported, logEvent } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB9DcOrFTtH_F7FYBJhHaCnOvsintmsK0Y",
-  authDomain: "pwas-507f1.firebaseapp.com",
-  projectId: "pwas-507f1",
-  storageBucket: "pwas-507f1.appspot.com",
-  messagingSenderId: "600478486707",
-  appId: "1:600478486707:web:a62dda40fc5e555ea560c6",
-  measurementId: "G-EHK640KPK5"
+            apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+            authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+            measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
